cd31590d4839fc77f3635231ac7bce0dd65c4fed
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-12 03:47:34 +0600

    I have started coding and making UI okay

--- Context API/src/App.jsx
+++ Context API/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import UserContextProvider from './context/UserContextProvider'
+import React from "react";
+import UserContextProvider from "./context/UserContextProvider";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <UserContextProvider>
-      <h1>React with Chai and share is important</h1>
+      <Login />
     </UserContextProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Context API/src/components/Login.jsx
+++ Context API/src/components/Login.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import UserContext from '../context/UserContext'
 
 const Login = () => {
-  return <div>
-    
-  </div>;
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = () => {
+
+  }
+
+  return (
+    <div>
+      <h2>Log In</h2>
+      <input 
+      value={username}
+      onChange={(e) => (setUsername(e.target.value))}
+      type="text" placeholder="Enter Your Name" />
+      <input 
+      value={password}
+      onChange={(e) => (setPassword(e.target.value))}
+      type="text" placeholder="Password" />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 };
 
 export default Login;
